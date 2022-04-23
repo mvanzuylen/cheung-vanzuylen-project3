17e85ab3d46132aed4ffaecc1bed177f3f0cb2b0
amlcheung <cheung.as@northeastern.edu>
2022-04-23 10:23:20 -0700

    file setup

---
+++ project3-review-app/src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+
+
+export default function App() {
+  return (
+    <div>Welcome To Restaurant Review App
+    </div>
+  );
+}
--- project3-review-app/src/index.js
+++ project3-review-app/src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import RestaurantEntry from './RestaurantEntry';
+import Login from './Login';
+import CreateUser from './CreateUser';
+import Header from './Header';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path={"/"} element={<App />}/>
+        <Route path={"/restaurant/:restaurantId"} element={<RestaurantEntry />}/>
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/createUser"} element={<CreateUser />} />
+    </Routes>
+    </BrowserRouter>
+      
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+// ReactDOM.render(
+//   <div>
+//   <BrowserRouter>
+//   <Header />
+//     <Routes>
+//       <Route path={"/"} element={<App />}/>
+//       {/* <Route path={"/restaurant/:restaurantId"} element={<RestaurantEntry />}/>
+//       <Route path={"/login"} element={<Login />} />
+//       <Route path={"/createUser"} element={<CreateUser />} /> */}
+//   </Routes>
+//   </BrowserRouter>
+      
+//   </div>
+// ,
+//   document.getElementById('root')
+// );
